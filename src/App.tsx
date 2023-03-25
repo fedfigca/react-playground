b867c6b8f5fb4b10fa6f72277ec5aaa44e7faefe
Federico Figueroa C <ffc.figueroa@gmail.com>
2023-03-25 11:06:32 -0600

    code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import { useEffect, useRef } from 'react'
 import { gsap } from 'gsap'
 import CssBaseline from '@mui/material/CssBaseline';
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const appContainer = useRef(null);
 
   useEffect(() => {
+    /**
+     * Fade in the entire app to make it
+     * easy on the eyes and encourage exploration
+     */
     gsap.fromTo(appContainer.current,
       {
         alpha: 0,
@@ -26,12 +28,6 @@ function App() {
     <div className='igalore' ref={appContainer}>
       <CssBaseline />
 
-      <a href="https://vitejs.dev" target="_blank">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://reactjs.org" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
     </div>
   )
 }
