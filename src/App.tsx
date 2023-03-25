7991f17341bd271bf063d19c4d368dcf9a409eb1
Federico Figueroa C <ffc.figueroa@gmail.com>
2023-03-25 10:56:09 -0600

    Installs GSAP and MUI Material

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useRef } from 'react'
+import { gsap } from 'gsap'
+import CssBaseline from '@mui/material/CssBaseline';
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const appContainer = useRef(null);
+
+  useEffect(() => {
+    gsap.fromTo(appContainer.current,
+      {
+        alpha: 0,
+      },
+      {
+        alpha: 1,
+        ease: 'none',
+        duration: 0.5,
+      }
+    )
+  }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='igalore' ref={appContainer}>
+      <CssBaseline />
+
+      <a href="https://vitejs.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://reactjs.org" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
     </div>
   )
 }
